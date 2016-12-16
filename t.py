89389e19530bbb80888ddf95adbce1263a9e65d3
uehara1414 <b1015062@fun.ac.jp>
2016-12-16 11:46:09 +0900

    Use genfromtxt

--- t.py
+++ t.py
@@ -1,29 +1,18 @@
+import scipy
 from scipy import stats
 import sys
-import csv
 
-def load(name):
-    l1 = []
-    l2 = []
-    with open(name, 'r') as fp:
-        reader = csv.reader(fp)
-
-        for row in reader:
-            if row[0]:
-                l1.append(float(row[0]))
-            if row[1]:
-                l2.append(float(row[1]))
-    return l1, l2
 
 def get_t_value_from_csv(csv_file):
-    l1, l2 = load(csv_file)
-    t, p = stats.ttest_rel(l1, l2)
+    data = scipy.genfromtxt(csv_file, delimiter=",")
+    t, p = scipy.stats.ttest_rel([x[0] for x in data], [x[1] for x in data])
     return t
 
 
 def main():
     t = get_t_value_from_csv(sys.argv[1])
     print(t)
+
 
 if __name__ == '__main__':
     main()
